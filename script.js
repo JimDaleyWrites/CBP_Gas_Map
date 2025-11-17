45c98ee6c8b511a48977a0a9b467b4477731ad48
JimDaleyWrites <jim.daley@southsideweekly.com>
2025-11-17 09:08:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN_HERE';
+
+const chapters = {
+  intro: {
+    center: [-87.623177, 41.881832],
+    zoom: 10,
+    popup: {
+      coords: [-87.623177, 41.881832],
+      html: `<p style="font-size: 16px; margin: 0;">A group of nine local newsrooms...</p>`
+    }
+  }
+  // Add the rest of your chapters here
+};
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/satellite-streets-v12',
+  center: chapters['intro'].center,
+  zoom: chapters['intro'].zoom
+});
+
+// ...rest of your JavaScript
