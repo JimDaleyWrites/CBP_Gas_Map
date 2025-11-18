eea2aa3da90f4146d767b7fac58a9b04a4c404d9
Maheen <maheen@Maheens-MacBook-Air.local>
2025-11-18 01:30:46 -0600

    changed structure a bit to separate story chapter content from design/formatting

---
+++ config.js
@@ -0,0 +1,307 @@
+var config = {
+    style: 'mapbox://styles/mapbox/satellite-streets-v12',
+    accessToken: 'pk.eyJ1IjoiamltZGFsZXkiLCJhIjoiY21oeHIzanN1MDRjZzJqcHYzOTI2ZHhnMiJ9.92tczXH-1swPAun1FrlfGw',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'light',
+    use3dTerrain: false,
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: 'TK footer',
+    chapters: [
+        {
+            id: 'intro',
+            alignment: 'right',
+            hidden: false,
+            title: 'Introduction',
+            image: '',
+            description: 'A group of nine local newsrooms...', 
+            location: {
+                center: [-87.623177, 41.881832],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location1',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 1',
+            image: '',
+            description: 'October 3 <br><br> Agents deployed one tear gas canister', 
+            location: {
+                center: [-87.7157113, 41.9173764],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location2',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 2',
+            image: '',
+            description: 'October 4 <br><br> Agents deployed one tear gas canister',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location3',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 3',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location4',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 4',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location5',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 5',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location6',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 6',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location7',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 7',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location8',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 8',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location9',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 9',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location10',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 10',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location11',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 11',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location12',
+            alignment: 'right',
+            hidden: false,
+            title: 'Location 12',
+            image: '',
+            description: '',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'location13',
+            alignment: 'left',
+            hidden: false,
+            title: 'Location 13',
+            image: '',
+            description: 'TK',
+            location: {
+                center: [-87.70444, 41.82259],
+                zoom: 15,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+            ],
+            onChapterExit: [
+            ]
+        },
+    ]
+};
